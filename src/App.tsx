939fff9f7405c2938d387ee06a580614cfe5eead
rm-reins <rm_reins@icloud.com>
2025-03-09 14:56:13 +0100

    feat: tailwindcss setup base

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {HomeLayout, Landing, Error, Register, Login, Products, SingleProduct, Cart, Checkout, Orders} from "./pages";
+import { Button } from "./components/button";
+import {useAppSelector} from "./hooks";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const {name} = useAppSelector((state) => state.userState);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <h1 className="text-7xl font-bold">App</h1>
+            <Button variant="destructive" size="lg" onClick={() => {
+                console.log("clicked");
+            }}>
+                Click me
+            </Button>
+            <Cart />
+        </div>
+    )
 }
 
-export default App
+export default App;
