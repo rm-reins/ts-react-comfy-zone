ad6a2d7a4ff795bd6e08cda2a9b9bc7bb2d8933c
rm-reins <rm_reins@icloud.com>
2025-03-11 00:47:36 +0100

    feat: cart button setup

--- src/components/ui/CartButton.tsx
+++ src/components/ui/CartButton.tsx
@@ -1,4 +1,24 @@
+import { Button } from "@/components";
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+
 function CartButton() {
-  return <div>CartButton</div>;
+  const numberOfItems = 10;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative shadow-none border-none"
+    >
+      <Link to="/cart">
+        <ShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numberOfItems}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 export default CartButton;
