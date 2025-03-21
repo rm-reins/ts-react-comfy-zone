a3366ba3eb5794477a2dea7395372825d64f2047
rm-reins <rm_reins@icloud.com>
2025-03-21 18:59:13 +0100

    refactor: move logger to utils

--- server/middleware/error-handler.ts
+++ server/middleware/error-handler.ts
@@ -1,56 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
-import winston from "winston";
 import { config } from "../config/index.js";
-import fs from "fs";
-import path from "path";
-
-const logDir = "logs";
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
-
-const logger = winston.createLogger({
-  level: config.environment === "production" ? "info" : "debug",
-
-  format: winston.format.combine(
-    winston.format.timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
-    winston.format.errors({ stack: true }),
-    winston.format.json()
-  ),
-
-  defaultMeta: { service: "comfy-zone-api" },
-
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.printf(
-          ({ timestamp, level, message, ...meta }) =>
-            `${timestamp} ${level}: ${message} ${
-              Object.keys(meta).length && meta.service !== "comfy-zone-api"
-                ? JSON.stringify(meta, null, 2)
-                : ""
-            }`
-        )
-      ),
-    }),
-
-    new winston.transports.File({
-      filename: path.join(
-        logDir,
-        `error-${new Date().toISOString().split("T")[0]}.log`
-      ),
-      level: "error",
-    }),
-    new winston.transports.File({
-      filename: path.join(
-        logDir,
-        `combined-${new Date().toISOString().split("T")[0]}.log`
-      ),
-    }),
-  ],
-});
+import { logger } from "../utils/logger.js";
 
 const errorHandlerMiddleware = (
   err: Error & {
